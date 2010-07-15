bcb4a17f5fe230fbfbd5f768b03e93133c1b1c20
Anders Boesen Lindbo Larsen <abll@diku.dk>
2010-07-16 01:50:20 +0200

    evaluation afsnit

--- tex/rapport/figures/plotter/rmsd.py
+++ tex/rapport/figures/plotter/rmsd.py
@@ -10,9 +10,9 @@ rmsd_data = []
 collisions_data = []
 rmsd_convergence_data = []
 
-print [l for _,l,_ in bending_stats[0][2]]
-length_avg = avg([l for _,l,_ in bending_stats[0][2]])
-print 'length_avg:',lengt_avg
+protein_lengths = [l for _,l,_ in bending_stats[0][3]]
+print 'protein_lengths_avg:', avg(protein_lengths),' protein_lengths_max',max(protein_lengths), '  protein_lengths_min',min(protein_lengths)
+
 
 for rotamer_depth, window_size, window_repetitions, proteins in bending_stats:	
 	print window_size, window_repetitions
@@ -40,63 +40,15 @@ xticks_convergence = range(1,len(rmsd_convergence_data[0])+1)
 print xticks_convergence 
 
 plots = [
-#{
-##	"title": "16 blocks, variating \# of threads",
-#	"file_name": "rmsd",
-#	"type": "graph", #= graph, bar
-##	"dimension_ratio": 0.75,  #defaults to golden ratio
-##	"legend_position": "upper right",
-#	"markers": ['-','--',':','-.','-',':','--','-.'],
-#	"plot_colors": ['0.0','0.0','0.0','0.0','0.8','0.8','0.8','0.8'],
-#	"axis_x_title": "window size, $w$",
-#	"axis_y_title": "RMSD (\AA)",
-#	"axis_x_scale": "linear", # = [linear], log, symlog
-#	"axis_y_scale": "linear", # = [linear], log, symlog
-##	"axis_x_lim": [1,24], #only for type graph
-##	"axis_y_lim": [1,300], #only for type graph
-##	"axis_x_ticks": [], #only for type graph
-##	"axis_y_ticks": range(0, 20001, 2500),
-##	"axis_x_tick_labels": [str(n+1) for n in range(8)], #only for type bar
-#	"data": [
-#		{
-#			"x": xticks,
-#			"y": rmsd_data,
-#		}
-#	]
-#},
-#{
-##	"title": "16 blocks, variating \# of threads",
-#	"file_name": "collisions",
-#	"type": "graph", #= graph, bar
-##	"dimension_ratio": 0.75,  #defaults to golden ratio
-##	"legend_position": "upper right",
-#	"markers": ['-','--',':','-.','-',':','--','-.'],
-#	"plot_colors": ['0.0','0.0','0.0','0.0','0.8','0.8','0.8','0.8'],
-#	"axis_x_title": "window size, $w$",
-#	"axis_y_title": "\# of collisions",
-#	"axis_x_scale": "linear", # = [linear], log, symlog
-#	"axis_y_scale": "linear", # = [linear], log, symlog
-##	"axis_x_lim": [1,24], #only for type graph
-##	"axis_y_lim": [1,300], #only for type graph
-##	"axis_x_ticks": [], #only for type graph
-##	"axis_y_ticks": range(0, 20001, 2500),
-##	"axis_x_tick_labels": [str(n+1) for n in range(8)], #only for type bar
-#	"data": [
-#		{
-#			"x": xticks,
-#			"y": collisions_data,
-#		}
-#	]
-#},
 {
 #	"title": "16 blocks, variating \# of threads",
-	"file_name": "rmsd_convergence",
+	"file_name": "plot_rmsd",
 	"type": "graph", #= graph, bar
 #	"dimension_ratio": 0.75,  #defaults to golden ratio
-	"legend_position": "upper right",
+#	"legend_position": "upper right",
 	"markers": ['-','--',':','-.','-',':','--','-.'],
 	"plot_colors": ['0.0','0.0','0.0','0.0','0.8','0.8','0.8','0.8'],
-	"axis_x_title": "algorithm repetitions",
+	"axis_x_title": "window size, $w$",
 	"axis_y_title": "RMSD (\AA)",
 	"axis_x_scale": "linear", # = [linear], log, symlog
 	"axis_y_scale": "linear", # = [linear], log, symlog
@@ -107,13 +59,61 @@ plots = [
 #	"axis_x_tick_labels": [str(n+1) for n in range(8)], #only for type bar
 	"data": [
 		{
+			"x": xticks,
+			"y": rmsd_data,
+		}
+	]
+},
+{
+#	"title": "16 blocks, variating \# of threads",
+	"file_name": "plot_collisions",
+	"type": "graph", #= graph, bar
+#	"dimension_ratio": 0.75,  #defaults to golden ratio
+#	"legend_position": "upper right",
+	"markers": ['-','--',':','-.','-',':','--','-.'],
+	"plot_colors": ['0.0','0.0','0.0','0.0','0.8','0.8','0.8','0.8'],
+	"axis_x_title": "window size, $w$",
+	"axis_y_title": "\# of collisions",
+	"axis_x_scale": "linear", # = [linear], log, symlog
+	"axis_y_scale": "linear", # = [linear], log, symlog
+#	"axis_x_lim": [1,24], #only for type graph
+#	"axis_y_lim": [1,300], #only for type graph
+#	"axis_x_ticks": [], #only for type graph
+#	"axis_y_ticks": range(0, 20001, 2500),
+#	"axis_x_tick_labels": [str(n+1) for n in range(8)], #only for type bar
+	"data": [
+		{
+			"x": xticks,
+			"y": collisions_data,
+		}
+	]
+},
+{
+#	"title": "16 blocks, variating \# of threads",
+	"file_name": "plot_rmsd_convergence",
+	"type": "graph", #= graph, bar
+#	"dimension_ratio": 0.75,  #defaults to golden ratio
+	"legend_position": "upper right",
+	"markers": ['-','--',':','-.','-',':','--','-.'],
+	"plot_colors": ['0.0','0.0','0.0','0.0','0.8','0.8','0.8','0.8'],
+	"axis_x_title": "\# of algorithm repetitions",
+	"axis_y_title": "RMSD (\AA)",
+	"axis_x_scale": "linear", # = [linear], log, symlog
+	"axis_y_scale": "linear", # = [linear], log, symlog
+#	"axis_x_lim": [1,24], #only for type graph
+#	"axis_y_lim": [1,300], #only for type graph
+#	"axis_x_ticks": [], #only for type graph
+#	"axis_y_ticks": range(0, 20001, 2500),
+#	"axis_x_tick_labels": [str(n+1) for n in range(8)], #only for type bar
+	"data": [
+		{
+			"label": "$w=2$",
+			"x": xticks_convergence,
+			"y": rmsd_convergence_data[1],
+		}, {
 			"label": "$w=8$",
 			"x": xticks_convergence,
 			"y": rmsd_convergence_data[7],
-		}, {
-			"label": "$w=16$",
-			"x": xticks_convergence,
-			"y": rmsd_convergence_data[15],
 		}, {
 			"label": "$w=20$",
 			"x": xticks_convergence,
